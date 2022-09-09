847036ea329f76b68b55e5bc2c3ea44031088e76
Rahim Nikfar <rahim.n3@gso.schule.koeln>
2022-09-09 12:20:48 +0200

    Operatoren Übungen Gemacht

--- 3DatentypenKonventieren/Program.cs
+++ 3DatentypenKonventieren/Program.cs
@@ -1,14 +1,36 @@
 ﻿
+//Aufgabe 1
 
 
+string a = "hallo";
+String b = "H";
+b += "allo";  
+
+Console.WriteLine(b);                 // = Hallo
+
+Console.WriteLine(b. Length);         //= 5
+
+Console.WriteLine(a == b);            //= False
+
+Console.WriteLine((object)a == b);    //= False
 
 
-//A3
-int DollarZahl = 36;
-Console.WriteLine(DollarZahl);
+//Aufgabe 2                                                          Erwartungen Ergebnisse,                      Nach der Überprüfung.
 
-char Dollar = Convert.ToChar(DollarZahl);
-Console.WriteLine(Dollar);
+double x = 2.0;                                                      
+double y = x * x + 1 + x / 4;                                        // 1,75                                     
 
-//Aufgabe 4
+double c = 2.0;
+double v = c * (c + 1 + c / 4);                                      // 2,5
 
+bool p = true && false || 5 > 6;                                     // False
+
+int j = 50;
+bool numeric = j > 47 && j < 58;                                     // True
+
+
+//Test Task 2
+Console.WriteLine(y);
+Console.WriteLine(v);
+Console.WriteLine(p);
+Console.WriteLine(numeric);
---
+++ Operatoren/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//Aufgabe 1
+
+
+string a = "hallo";
+String b = "H";
+b += "allo";
+
+Console.WriteLine(b);                 // = Hallo
+
+Console.WriteLine(b.Length);         //= 5
+
+Console.WriteLine(a == b);            //= False
+
+Console.WriteLine((object)a == b);    //= False
+
+
+//Aufgabe 2                                                          Erwartungen Ergebnisse,                      Nach der Überprüfung.
+
+double x = 2.0;
+double y = x * x + 1 + x / 4;                                        // 1,75                                       5,5
+
+double c = 2.0;
+double v = c * (c + 1 + c / 4);                                      // 2,5                                        7
+
+bool p = true && false || 5 > 6;                                     // False                                      False
+
+int j = 50;
+bool numeric = j > 47 && j < 58;                                     // True                                       True
+
+//Test Task 2
+Console.WriteLine(y);
+Console.WriteLine(v);
+Console.WriteLine(p);
+Console.WriteLine(numeric);
